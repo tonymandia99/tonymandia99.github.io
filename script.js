698b77ecf3191117dfafca1029d2b7bb38838693
Antonio Mandia Jr <tonymandia99@gmail.com>
2025-08-29 14:35:01 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const typingElement = document.querySelector('.typing');
+const words = ["Data Automation Engineer", "Python Developer", "Prompt Engineer", "Data Analyst"];
+let wordIndex = 0;
+let charIndex = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function typeEffect() {
+  currentWord = words[wordIndex];
+  if (isDeleting) {
+    typingElement.textContent = currentWord.substring(0, charIndex--);
+  } else {
+    typingElement.textContent = currentWord.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1000);
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeEffect, 500);
+  } else {
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+  }
+}
+typeEffect();
